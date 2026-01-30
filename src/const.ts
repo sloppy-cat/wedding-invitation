a88f70da03c9303f3e814db20c9ac1343e01ab06
sloppy-cat <js38911@naver.com>
2026-01-31 01:25:13 +0900

    지도패치

--- src/const.ts
+++ src/const.ts
@@ -25,17 +25,17 @@ export const SHARE_ADDRESS = LOCATION
 export const SHARE_ADDRESS_TITLE = LOCATION
 
 // 네이버 지도 및 카카오 네비게이션에 사용할 좌표. [경도, 위도] 형식.
-export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
+export const WEDDING_HALL_POSITION = [ 127.111428, 37.400642]
 
 // 네이버 지도의 웨딩홀 장소 ID
 // 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
 // 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
-export const NMAP_PLACE_ID = 13321741
+export const NMAP_PLACE_ID = 36301790
 
 // 카카오 지도의 웨딩홀 장소 ID
 // 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
 // 예: https://place.map.kakao.com/8634826 -> 8634826
-export const KMAP_PLACE_ID = 8634826
+export const KMAP_PLACE_ID = 26903392
 
 export const BRIDE_FULLNAME = "임채영"
 export const BRIDE_FIRSTNAME = "채영"
